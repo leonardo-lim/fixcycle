7284b45b73a27939f9ab1289f04d419de4d35ed3
leonardolim78 <leonardolim178@gmail.com>
2022-06-02 14:48:22 +0700

    Add event listener for get started button

--- app/src/main/java/com/leo/fixcycle/activities/StartActivity.java
+++ app/src/main/java/com/leo/fixcycle/activities/StartActivity.java
@@ -2,7 +2,9 @@ package com.leo.fixcycle.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.leo.fixcycle.R;
 
@@ -12,5 +14,14 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button getStartedButton = findViewById(R.id.get_started_btn);
+        getStartedButton.setOnClickListener(view -> openActivity());
+    }
+
+    private void openActivity() {
+        Intent intent = new Intent(StartActivity.this, LoadingActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
