8d927cfc07ca0579d76897858f44cb982718c4d4
leonardolim78 <leonardolim178@gmail.com>
2022-06-02 14:52:47 +0700

    Add handler for delayed activity opening

--- app/src/main/java/com/leo/fixcycle/activities/LoadingActivity.java
+++ app/src/main/java/com/leo/fixcycle/activities/LoadingActivity.java
@@ -2,7 +2,9 @@ package com.leo.fixcycle.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.leo.fixcycle.R;
 
@@ -12,5 +14,14 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this::openActivity,2000L);
+    }
+
+    private void openActivity() {
+        Intent intent = new Intent(LoadingActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
