2190061682ac7c9a5318c0abd9933228a062c51a
leonardolim78 <leonardolim178@gmail.com>
2022-06-06 11:30:28 +0700

    Add fragments for all bottom navigation

--- app/src/main/java/com/leo/fixcycle/activities/MainActivity.java
+++ app/src/main/java/com/leo/fixcycle/activities/MainActivity.java
@@ -1,16 +1,53 @@
 package com.leo.fixcycle.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.leo.fixcycle.R;
+import com.leo.fixcycle.databinding.ActivityMainBinding;
+import com.leo.fixcycle.fragments.BookServiceFragment;
+import com.leo.fixcycle.fragments.HomeFragment;
+import com.leo.fixcycle.fragments.MyMotorcycleFragment;
+import com.leo.fixcycle.fragments.OrdersFragment;
+import com.leo.fixcycle.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.navView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home_nav) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.orders_nav) {
+                replaceFragment(new OrdersFragment());
+            } else if (itemId == R.id.book_service_nav) {
+                replaceFragment(new BookServiceFragment());
+            } else if (itemId == R.id.my_moto_nav) {
+                replaceFragment(new MyMotorcycleFragment());
+            } else {
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
