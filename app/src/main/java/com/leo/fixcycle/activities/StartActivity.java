e9eeed2d5b94ec8594f6670462d7f641cc253d36
leonardolim78 <leonardolim178@gmail.com>
2022-06-10 00:03:01 +0700

    Show start page only on first time open

--- app/src/main/java/com/leo/fixcycle/activities/StartActivity.java
+++ app/src/main/java/com/leo/fixcycle/activities/StartActivity.java
@@ -2,7 +2,9 @@ package com.leo.fixcycle.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -15,8 +17,21 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        Button getStartedButton = findViewById(R.id.get_started_btn);
-        getStartedButton.setOnClickListener(view -> openActivity());
+        SharedPreferences sp = getSharedPreferences("AppPrefs", Context.MODE_PRIVATE);
+        String firstTime = sp.getString("isFirstTime", "");
+
+        if (firstTime.equals("true")) {
+            Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("isFirstTime", "true");
+            editor.apply();
+
+            Button getStartedButton = findViewById(R.id.get_started_btn);
+            getStartedButton.setOnClickListener(view -> openActivity());
+        }
     }
 
     private void openActivity() {
