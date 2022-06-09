707a21c1dc1cca761d844c69cf722d4e3005b269
AndrosC <androscc7@gmail.com>
2022-06-09 23:59:18 +0700

    Add onClickListener to motorcycle adapter

--- app/src/main/java/com/leo/fixcycle/adapters/MotorcycleAdapter.java
+++ app/src/main/java/com/leo/fixcycle/adapters/MotorcycleAdapter.java
@@ -18,17 +18,18 @@ import java.util.ArrayList;
 public class MotorcycleAdapter extends RecyclerView.Adapter<MotorcycleAdapter.myviewholder>{
 
     ArrayList<Motorcycle> motorcyclesDataHolder;
-    private int limit = 4;
+    OnClickShowListener mOnClickShowListener;
 
-    public MotorcycleAdapter(ArrayList<Motorcycle> motorcyclesDataHolder) {
+    public MotorcycleAdapter(ArrayList<Motorcycle> motorcyclesDataHolder, OnClickShowListener onClickShowListener) {
         this.motorcyclesDataHolder = motorcyclesDataHolder;
+        this.mOnClickShowListener = onClickShowListener;
     }
 
     @NonNull
     @Override
     public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row_motorcycle,parent, false);
-        return new myviewholder(view);
+        return new myviewholder(view, mOnClickShowListener);
     }
 
     @Override
@@ -37,7 +38,6 @@ public class MotorcycleAdapter extends RecyclerView.Adapter<MotorcycleAdapter.my
         holder.name.setText(motorcyclesDataHolder.get(position).getName());
         holder.brand.setText(motorcyclesDataHolder.get(position).getBrand());
         holder.licensePlate.setText(motorcyclesDataHolder.get(position).getLicensePlate());
-
     }
 
     @Override
@@ -45,16 +45,27 @@ public class MotorcycleAdapter extends RecyclerView.Adapter<MotorcycleAdapter.my
         return motorcyclesDataHolder.size();
     }
 
-    class myviewholder extends RecyclerView.ViewHolder {
+    class myviewholder extends RecyclerView.ViewHolder implements View.OnClickListener{
         ImageView img;
         TextView name,brand,licensePlate;
-
-        public myviewholder(@NonNull View itemView){
+        OnClickShowListener onClickShowListener;
+        public myviewholder(@NonNull View itemView, OnClickShowListener onClickShowListener){
             super(itemView);
             img= itemView.findViewById(R.id.motorcycle_img);
             name =itemView.findViewById(R.id.motorcycle_name);
             brand= itemView.findViewById(R.id.motorcycle_brand);
             licensePlate=itemView.findViewById(R.id.motorcycle_license_plate);
+            this.onClickShowListener = onClickShowListener;
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            onClickShowListener.onClickShowListener(getAdapterPosition());
+        }
+    }
+
+    public interface OnClickShowListener{
+        void onClickShowListener(int position);
     }
 }
