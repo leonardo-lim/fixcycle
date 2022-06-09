e8de1404b82af7a27be5ef73abad216c40b14a02
leonardolim78 <leonardolim178@gmail.com>
2022-06-09 22:13:47 +0700

    Add motorcycle data input activity

---
+++ app/src/main/java/com/leo/fixcycle/activities/MotorcycleDataInputActivity.java
@@ -0,0 +1,16 @@
+package com.leo.fixcycle.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leo.fixcycle.R;
+
+public class MotorcycleDataInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motorcycle_data_input);
+    }
+}
