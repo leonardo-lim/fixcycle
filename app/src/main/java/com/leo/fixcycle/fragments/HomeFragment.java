34ed2b83863e3ecc2eabf8f0eb0db709a9ceb77d
leonardolim78 <leonardolim178@gmail.com>
2022-06-10 23:58:59 +0700

    Get user name and handle logged in user

--- app/src/main/java/com/leo/fixcycle/fragments/HomeFragment.java
+++ app/src/main/java/com/leo/fixcycle/fragments/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.leo.fixcycle.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +10,71 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.leo.fixcycle.R;
+import com.leo.fixcycle.activities.LoginActivity;
+import com.leo.fixcycle.models.User;
+import com.leo.fixcycle.networks.UserClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        if (getActivity() != null) {
+            SharedPreferences sp = getActivity().getApplicationContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+            String accessToken = sp.getString("accessToken", "");
+
+            TextView welcomeText = view.findViewById(R.id.welcome_text);
+            UserClient call = new UserClient();
+
+            call.getApi().getUser("Bearer " + accessToken).enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        String name = response.body().getData().getUser().getName();
+                        welcomeText.setText("Hello " + name + "!");
+                    } else if (response.code() == 401) {
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
+
+                        if (getActivity() != null) {
+                            getActivity().finish();
+                        }
+                    } else if (response.errorBody() != null) {
+                        try {
+                            JSONObject error = new JSONObject(response.errorBody().string());
+                            String message = error.getString("message");
+                            showToast(message);
+                        } catch (IOException | JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+                    t.printStackTrace();
+                }
+            });
+        }
+
+        return view;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
     }
 }
