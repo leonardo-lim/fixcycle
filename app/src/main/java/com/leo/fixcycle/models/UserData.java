effc00c924d15234d364ba336d3ddf5626ab3976
leonardolim78 <leonardolim178@gmail.com>
2022-06-10 23:55:24 +0700

    Add user profile model

--- app/src/main/java/com/leo/fixcycle/models/UserData.java
+++ app/src/main/java/com/leo/fixcycle/models/UserData.java
@@ -2,12 +2,21 @@ package com.leo.fixcycle.models;
 
 public class UserData {
     private String accessToken;
+    private UserDataUser user;
 
     public String getAccessToken() {
         return accessToken;
     }
 
+    public UserDataUser getUser() {
+        return user;
+    }
+
     public void setAccessToken(String accessToken) {
         this.accessToken = accessToken;
+    }
+
+    public void setUser(UserDataUser user) {
+        this.user = user;
     }
 }
