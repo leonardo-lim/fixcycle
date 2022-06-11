24424c9748b232c115de9521757e7b8b577f250d
AndrosC <androscc7@gmail.com>
2022-06-11 23:26:12 +0700

    Complete my motorcycle fragment logic

--- app/src/main/java/com/leo/fixcycle/fragments/MyMotorcycleFragment.java
+++ app/src/main/java/com/leo/fixcycle/fragments/MyMotorcycleFragment.java
@@ -15,8 +15,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.material.tabs.TabLayout;
 import com.leo.fixcycle.R;
+import com.leo.fixcycle.activities.AddMotorcycleActivity;
 import com.leo.fixcycle.activities.MotorcycleDetailsActivity;
 import com.leo.fixcycle.models.Motorcycle;
 import com.leo.fixcycle.adapters.MotorcycleAdapter;
@@ -37,7 +37,10 @@ public class MyMotorcycleFragment extends Fragment implements MotorcycleAdapter.
         button =  view.findViewById(R.id.add_motorcycle_btn);
         recyclerView = view.findViewById(R.id.mymoto_recycle_view);
 
-
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AddMotorcycleActivity.class);
+            startActivity(intent);
+        });
 
         insertMotor();
 
@@ -51,22 +54,22 @@ public class MyMotorcycleFragment extends Fragment implements MotorcycleAdapter.
     public void insertMotor(){
         motorcycleDataHolder = new ArrayList<>();
 
-        Motorcycle motor1 = new Motorcycle(R.drawable.logo,1,"Motorola","392838","Honda","Motor mainan","Duamax","Gradien blue ocean");
+        Motorcycle motor1 = new Motorcycle(R.drawable.logo,1,"Motorola","D 2323 FX","Honda","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
         motorcycleDataHolder.add(motor1);
 
-        Motorcycle motor2 = new Motorcycle(R.drawable.logo,1,"Motorola","392838","Honda","Motor mainan","Duamax","Gradien blue ocean");
+        Motorcycle motor2 = new Motorcycle(R.drawable.logo,1,"CBR 150 KW","D 3423 MX","Kawasaki","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
         motorcycleDataHolder.add(motor2);
 
-        Motorcycle motor3 = new Motorcycle(R.drawable.logo,1,"Motorola","392838","Honda","Motor mainan","Duamax","Gradien blue ocean");
+        Motorcycle motor3 = new Motorcycle(R.drawable.logo,1,"R15","F 233 L","Yamaha","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
         motorcycleDataHolder.add(motor3);
 
-        Motorcycle motor4 = new Motorcycle(R.drawable.logo,1,"Motorola","392838","Honda","Motor mainan","Duamax","Gradien blue ocean");
+        Motorcycle motor4 = new Motorcycle(R.drawable.logo,1,"PCX","392838","Honda","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
         motorcycleDataHolder.add(motor4);
 
-        Motorcycle motor5 = new Motorcycle(R.drawable.logo,1,"CBR 150 XRS","392838","KawasakingRX","Motor mainan","Duamax","Gradien blue ocean");
+        Motorcycle motor5 = new Motorcycle(R.drawable.logo,1,"CBR 150 XRS","392838","KawasakingRX","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
         motorcycleDataHolder.add(motor5);
 
-        Motorcycle motor6 = new Motorcycle(R.drawable.logo,1,"Motorola","392838","Kawasaki","Motor mainan","Duamax","Gradien blue ocean");
+        Motorcycle motor6 = new Motorcycle(R.drawable.logo,1,"Motorola","392838","Kawasaki","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
         motorcycleDataHolder.add(motor6);
         motorcycleDataHolder.add(motor6);
         motorcycleDataHolder.add(motor6);
@@ -83,18 +86,23 @@ public class MyMotorcycleFragment extends Fragment implements MotorcycleAdapter.
         int image = motorcycleDataHolder.get(position).getImage();
         int cylinderCapacity = motorcycleDataHolder.get(position).getCylinderCapacity();
         String name = motorcycleDataHolder.get(position).getName();
+        String type = motorcycleDataHolder.get(position).getType();
         String licensePlate = motorcycleDataHolder.get(position).getLicensePlate();
-        String brand = motorcycleDataHolder.get(position).getType();
+        String brand = motorcycleDataHolder.get(position).getBrand();
         String fuelType = motorcycleDataHolder.get(position).getFuelType();
         String color = motorcycleDataHolder.get(position).getColor();
+        String productionYear = motorcycleDataHolder.get(position).getDate();
 
         Intent intent =  new Intent(getActivity(), MotorcycleDetailsActivity.class);
         intent.putExtra("name", name.toString());
-//        intent.putExtra("image", image);
-//        intent.putExtra("name", name.toString());
-//        intent.putExtra("name", name.toString());
-//        intent.putExtra("name", name.toString());
-//        intent.putExtra("name", name.toString());
+        intent.putExtra("image", image);
+        intent.putExtra("brand", brand.toString());
+        intent.putExtra("type", type.toString());
+        intent.putExtra("license_plate", licensePlate.toString());
+        intent.putExtra("cylinder_capacity", cylinderCapacity);
+        intent.putExtra("color", color);
+        intent.putExtra("fuel_type", fuelType);
+        intent.putExtra("production_year",productionYear);
         startActivity(intent);
     }
 }
