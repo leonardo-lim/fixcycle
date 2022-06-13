bd4f2b820840b73f859902c7d8f417fc0047353a
leonardolim78 <leonardolim178@gmail.com>
2022-06-14 00:08:15 +0700

    Add routing for get motorcycle by id, motorcycle edit, and motorcycle delete request

--- app/src/main/java/com/leo/fixcycle/networks/MotorcycleService.java
+++ app/src/main/java/com/leo/fixcycle/networks/MotorcycleService.java
@@ -4,14 +4,26 @@ import com.leo.fixcycle.models.Motorcycle;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface MotorcycleService {
     @GET("motorcycles/user")
     Call<Motorcycle> getMotorcycle(@Header("Authorization") String accessToken);
 
+    @GET("motorcycles/{id}")
+    Call<Motorcycle> getMotorcycle(@Path("id") int motorcycleId, @Header("Authorization") String accessToken);
+
     @POST("motorcycles")
     Call<Motorcycle> saveMotorcycle(@Body Motorcycle motorcycle, @Header("Authorization") String accessToken);
+
+    @PUT("motorcycles/{id}")
+    Call<Motorcycle> updateMotorcycle(@Path("id") int motorcycleId, @Body Motorcycle motorcycle, @Header("Authorization") String accessToken);
+
+    @DELETE("motorcycles/{id}")
+    Call<Motorcycle> removeMotorcycle(@Path("id") int motorcycleId, @Header("Authorization") String accessToken);
 }
