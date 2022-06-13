01bb3cf45ea99f99d863f6c0612cb050011ef4a7
leonardolim78 <leonardolim178@gmail.com>
2022-06-14 00:05:59 +0700

    Add motorcycle data model property

--- app/src/main/java/com/leo/fixcycle/models/MotorcycleData.java
+++ app/src/main/java/com/leo/fixcycle/models/MotorcycleData.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class MotorcycleData {
     private List<MotorcycleDataMotorcycle> motorcycles;
+    private MotorcycleDataMotorcycle motorcycle;
 
     public List<MotorcycleDataMotorcycle> getMotorcycles() {
         return motorcycles;
@@ -11,5 +12,13 @@ public class MotorcycleData {
 
     public void setMotorcycles(List<MotorcycleDataMotorcycle> motorcycles) {
         this.motorcycles = motorcycles;
+    }
+
+    public MotorcycleDataMotorcycle getMotorcycle() {
+        return motorcycle;
+    }
+
+    public void setMotorcycle(MotorcycleDataMotorcycle motorcycle) {
+        this.motorcycle = motorcycle;
     }
 }
