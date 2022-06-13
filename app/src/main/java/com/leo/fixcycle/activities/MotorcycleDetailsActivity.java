eb0ed6b61369b7c6c6e1d1f3984852f550e537b1
androschen <androscc7@gmail.com>
2022-06-13 14:31:56 +0700

    Fix motorcycle details activity

--- app/src/main/java/com/leo/fixcycle/activities/MotorcycleDetailsActivity.java
+++ app/src/main/java/com/leo/fixcycle/activities/MotorcycleDetailsActivity.java
@@ -9,8 +9,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.leo.fixcycle.R;
+import com.leo.fixcycle.models.MotorcycleDataMotorcycle;
 
 public class MotorcycleDetailsActivity extends AppCompatActivity {
+    MotorcycleDataMotorcycle motorcycleDataMotorcycle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,27 +27,32 @@ public class MotorcycleDetailsActivity extends AppCompatActivity {
         TextView cylinderCapacityContent = findViewById(R.id.cylinder_capacity_content);
         TextView colorContent = findViewById(R.id.color_content);
         TextView fuelTypeContent = findViewById(R.id.fuel_type_content);
-        TextView dateContent = findViewById(R.id.production_year_content);
+        TextView productionYearContent = findViewById(R.id.production_year_content);
 
         Intent intent = getIntent();
-        int image = intent.getIntExtra("image",0);
-        String name = intent.getStringExtra("name");
-        String brand = intent.getStringExtra("brand");
-        String type = intent.getStringExtra("type");
-        int cylinderCapacity = intent.getIntExtra("cylinder_capacity",0);
-        String licensePlate = intent.getStringExtra("license_plate");
-        String color = intent.getStringExtra("color");
-        String fuelType = intent.getStringExtra("fuel_type");
-        String date = intent.getStringExtra("production_year");
+        if(intent.getExtras()!=null){
+            motorcycleDataMotorcycle = (MotorcycleDataMotorcycle) intent.getSerializableExtra("data");
 
-        imageView.setImageResource(image);
-        nameContent.setText(name);
-        brandContent.setText(brand);
-        typeContent.setText(type);
-        cylinderCapacityContent.setText(String.valueOf(cylinderCapacity));
-        licenseContent.setText(licensePlate);
-        colorContent.setText(color);
-        fuelTypeContent.setText(fuelType);
-        dateContent.setText(date);
+            String name = motorcycleDataMotorcycle.getName();
+            String brand = motorcycleDataMotorcycle.getBrand();
+            String type = motorcycleDataMotorcycle.getType();
+            double cylinderCapacity = motorcycleDataMotorcycle.getCylinderCapacity();
+            String licensePlate = motorcycleDataMotorcycle.getLicensePlate();
+            String color = motorcycleDataMotorcycle.getColor();
+            String fuelType = motorcycleDataMotorcycle.getFuelType();
+            String productionYear = motorcycleDataMotorcycle.getProductionYear();
+            productionYear = productionYear.substring(0,4);
+
+            imageView.setImageResource(R.drawable.start);
+            nameContent.setText(name);
+            brandContent.setText(brand);
+            typeContent.setText(type);
+            cylinderCapacityContent.setText(String.valueOf(cylinderCapacity));
+            licenseContent.setText(licensePlate);
+            colorContent.setText(color);
+            fuelTypeContent.setText(fuelType);
+            productionYearContent.setText(productionYear);
+        }
+
     }
 }
