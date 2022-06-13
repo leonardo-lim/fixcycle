f6a28183d28670bfeb0ca55b6e12626a1b81e731
androschen <androscc7@gmail.com>
2022-06-13 14:33:47 +0700

    Fix my motorcycle fragment

--- app/src/main/java/com/leo/fixcycle/fragments/MyMotorcycleFragment.java
+++ app/src/main/java/com/leo/fixcycle/fragments/MyMotorcycleFragment.java
@@ -1,34 +1,45 @@
 package com.leo.fixcycle.fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.leo.fixcycle.R;
 import com.leo.fixcycle.activities.AddMotorcycleActivity;
+import com.leo.fixcycle.activities.LoginActivity;
 import com.leo.fixcycle.activities.MotorcycleDetailsActivity;
 import com.leo.fixcycle.models.Motorcycle;
 import com.leo.fixcycle.adapters.MotorcycleAdapter;
+import com.leo.fixcycle.models.MotorcycleDataMotorcycle;
+import com.leo.fixcycle.networks.MotorcycleClient;
 
-import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MyMotorcycleFragment extends Fragment implements MotorcycleAdapter.OnClickShowListener{
     RecyclerView recyclerView;
-    ArrayList<Motorcycle> motorcycleDataHolder;
+    List<MotorcycleDataMotorcycle> motorcycleDataHolder;
     View view;
     Button button;
-
+    MotorcycleAdapter motorcycleAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,59 +53,65 @@ public class MyMotorcycleFragment extends Fragment implements MotorcycleAdapter.
             startActivity(intent);
         });
 
-        insertMotor();
-
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new MotorcycleAdapter(motorcycleDataHolder, this));
+        motorcycleAdapter= new MotorcycleAdapter(motorcycleDataHolder,this);
+        getMotorcycle(view);
+
 
         return view;
     }
 
+    private void getMotorcycle(View view){
+        if (getActivity() != null) {
+            SharedPreferences sp = getActivity().getApplicationContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+            String accessToken = sp.getString("accessToken", "");
 
-    public void insertMotor(){
-        motorcycleDataHolder = new ArrayList<>();
+            MotorcycleClient call = new MotorcycleClient();
 
-        Motorcycle motor1 = new Motorcycle(R.drawable.logo,1,"Motorola","D 2323 FX","Honda","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
-        motorcycleDataHolder.add(motor1);
+            call.getApi().getMotorcycle("Bearer " + accessToken).enqueue(new Callback<Motorcycle>() {
+                @Override
+                public void onResponse(Call<Motorcycle> call, Response<Motorcycle> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        List<MotorcycleDataMotorcycle> motorcyclesDataHolder = response.body().getData().getMotorcycles();
+                        motorcycleAdapter.setData(motorcyclesDataHolder);
+                        recyclerView.setAdapter(motorcycleAdapter);
 
-        Motorcycle motor2 = new Motorcycle(R.drawable.logo,1,"CBR 150 KW","D 3423 MX","Kawasaki","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
-        motorcycleDataHolder.add(motor2);
+                    } else if (response.code() == 401) {
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        startActivity(intent);
 
-        Motorcycle motor3 = new Motorcycle(R.drawable.logo,1,"R15","F 233 L","Yamaha","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
-        motorcycleDataHolder.add(motor3);
+                        if (getActivity() != null) {
+                            getActivity().finish();
+                        }
+                    } else if (response.errorBody() != null) {
+                        try {
+                            JSONObject error = new JSONObject(response.errorBody().string());
+                            String message = error.getString("message");
+                            showToast(message);
+                        } catch (IOException | JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
 
-        Motorcycle motor4 = new Motorcycle(R.drawable.logo,1,"PCX","392838","Honda","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
-        motorcycleDataHolder.add(motor4);
-
-        Motorcycle motor5 = new Motorcycle(R.drawable.logo,1,"CBR 150 XRS","392838","KawasakingRX","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
-        motorcycleDataHolder.add(motor5);
-
-        Motorcycle motor6 = new Motorcycle(R.drawable.logo,1,"Motorola","392838","Kawasaki","Motor mainan","Duamax","Gradien blue ocean","2018-01-01 00:00");
-        motorcycleDataHolder.add(motor6);
+                @Override
+                public void onFailure(Call<Motorcycle> call, Throwable t) {
+                    t.printStackTrace();
+                }
+            });
+        }
     }
 
     @Override
-    public void onClickShowListener(int position) {
-        int image = motorcycleDataHolder.get(position).getImage();
-        int cylinderCapacity = motorcycleDataHolder.get(position).getCylinderCapacity();
-        String name = motorcycleDataHolder.get(position).getName();
-        String type = motorcycleDataHolder.get(position).getType();
-        String licensePlate = motorcycleDataHolder.get(position).getLicensePlate();
-        String brand = motorcycleDataHolder.get(position).getBrand();
-        String fuelType = motorcycleDataHolder.get(position).getFuelType();
-        String color = motorcycleDataHolder.get(position).getColor();
-        String productionYear = motorcycleDataHolder.get(position).getDate();
+    public void onClickShowListener(MotorcycleDataMotorcycle motorcycleDataMotorcycle) {
+        Intent intent =  new Intent(getActivity(), MotorcycleDetailsActivity.class).putExtra("data",motorcycleDataMotorcycle);
 
-        Intent intent =  new Intent(getActivity(), MotorcycleDetailsActivity.class);
-        intent.putExtra("name", name.toString());
-        intent.putExtra("image", image);
-        intent.putExtra("brand", brand.toString());
-        intent.putExtra("type", type.toString());
-        intent.putExtra("license_plate", licensePlate.toString());
-        intent.putExtra("cylinder_capacity", cylinderCapacity);
-        intent.putExtra("color", color);
-        intent.putExtra("fuel_type", fuelType);
-        intent.putExtra("production_year",productionYear);
         startActivity(intent);
     }
+
+    private void showToast(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+    }
+
+
 }
