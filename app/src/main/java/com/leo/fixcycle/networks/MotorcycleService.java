919ef2f5df9383b4260b9eb46981849718ce70a0
androschen <androscc7@gmail.com>
2022-06-13 14:33:03 +0700

    Add routing for get motorcycle request

--- app/src/main/java/com/leo/fixcycle/networks/MotorcycleService.java
+++ app/src/main/java/com/leo/fixcycle/networks/MotorcycleService.java
@@ -4,10 +4,14 @@ import com.leo.fixcycle.models.Motorcycle;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface MotorcycleService {
+    @GET("motorcycles/user")
+    Call<Motorcycle> getMotorcycle(@Header("Authorization") String accessToken);
+
     @POST("motorcycles")
     Call<Motorcycle> saveMotorcycle(@Body Motorcycle motorcycle, @Header("Authorization") String accessToken);
 }
