e1d98c8b0b2f49eb0357fed157485fb7dd09193d
leonardolim78 <leonardolim178@gmail.com>
2022-06-15 18:08:39 +0700

    Add service client network

---
+++ app/src/main/java/com/leo/fixcycle/networks/ServiceClient.java
@@ -0,0 +1,17 @@
+package com.leo.fixcycle.networks;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceClient {
+    private static final String BASE_URL = "http://10.0.2.2:5000/api/v1/";
+
+    public ServiceService getApi() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit.create(ServiceService.class);
+    }
+}
