5714fc5ee51c6d9faaf6f58473c0bf0d2a915b72
androschen <androscc7@gmail.com>
2022-06-16 10:03:41 +0700

    Add post to service interface

--- app/src/main/java/com/leo/fixcycle/networks/ServiceService.java
+++ app/src/main/java/com/leo/fixcycle/networks/ServiceService.java
@@ -3,10 +3,15 @@ package com.leo.fixcycle.networks;
 import com.leo.fixcycle.models.Service;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface ServiceService {
     @GET("services/user")
     Call<Service> getService(@Header("Authorization") String accessToken);
+
+    @POST("services")
+    Call<Service> saveService(@Body Service service, @Header("Authorization") String accessToken);
 }
