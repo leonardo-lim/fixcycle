d3581a560d1f3d2409cf7f97b70a4c62df3a9d6a
leonardo-lim <leonardolim178@gmail.com>
2022-06-18 22:51:41 +0700

    Add routing for get all services and update service by id

--- app/src/main/java/com/leo/fixcycle/networks/ServiceService.java
+++ app/src/main/java/com/leo/fixcycle/networks/ServiceService.java
@@ -7,11 +7,19 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ServiceService {
+    @GET("services")
+    Call<Service> getServices(@Header("Authorization") String accessToken);
+
     @GET("services/user")
     Call<Service> getService(@Header("Authorization") String accessToken);
 
     @POST("services")
     Call<Service> saveService(@Body Service service, @Header("Authorization") String accessToken);
+
+    @PUT("services/{id}")
+    Call<Service> updateService(@Path("id") int serviceId, @Body Service service, @Header("Authorization") String accessToken);
 }
