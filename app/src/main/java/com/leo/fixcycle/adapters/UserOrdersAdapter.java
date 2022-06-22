00fa544f02e5d0ef29fdcb867e982457a8e9a989
androschen <androscc7@gmail.com>
2022-06-22 14:41:55 +0700

    Add user orders adapter

---
+++ app/src/main/java/com/leo/fixcycle/adapters/UserOrdersAdapter.java
@@ -0,0 +1,86 @@
+package com.leo.fixcycle.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.leo.fixcycle.R;
+import com.leo.fixcycle.models.MotorcycleDataMotorcycle;
+import com.leo.fixcycle.models.ServiceDataService;
+
+
+import java.util.List;
+
+public class UserOrdersAdapter extends RecyclerView.Adapter<UserOrdersAdapter.ServiceOrdersViewHolder> {
+    List<MotorcycleDataMotorcycle> motorcyclesDataHolder;
+    List<ServiceDataService> servicesDataHolder;
+    OnClickOrdersListener onClickOrdersListener;
+
+    public UserOrdersAdapter(List<MotorcycleDataMotorcycle> motorcyclesDataHolder, List<ServiceDataService> servicesDataHolder, OnClickOrdersListener onClickOrdersListener) {
+        this.motorcyclesDataHolder = motorcyclesDataHolder;
+        this.servicesDataHolder = servicesDataHolder;
+        this.onClickOrdersListener = onClickOrdersListener;
+    }
+
+    @NonNull
+    @Override
+    public ServiceOrdersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_row_motorcycle, parent, false);
+        return new ServiceOrdersViewHolder(view, onClickOrdersListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserOrdersAdapter.ServiceOrdersViewHolder holder, int position) {
+        MotorcycleDataMotorcycle res = motorcyclesDataHolder.get(position);
+        holder.img.setImageResource(R.drawable.start);
+        holder.name.setText(res.getName());
+        holder.brand.setText(res.getBrand());
+        holder.licensePlate.setText(res.getLicensePlate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return motorcyclesDataHolder.size();
+    }
+
+    class ServiceOrdersViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView img;
+        TextView name, brand, licensePlate;
+        OnClickOrdersListener onClickOrdersListener;
+
+        public ServiceOrdersViewHolder(@NonNull View itemView, OnClickOrdersListener onClickOrdersListener){
+            super(itemView);
+
+            this.onClickOrdersListener = onClickOrdersListener;
+
+            img= itemView.findViewById(R.id.motorcycle_img);
+            name =itemView.findViewById(R.id.motorcycle_name);
+            brand= itemView.findViewById(R.id.motorcycle_brand);
+            licensePlate=itemView.findViewById(R.id.motorcycle_license_plate);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            onClickOrdersListener.onClickShowListener(motorcyclesDataHolder.get(position), servicesDataHolder.get(position));
+        }
+    }
+
+    public interface OnClickOrdersListener {
+        void onClickShowListener(MotorcycleDataMotorcycle motorcycleDataMotorcycle, ServiceDataService serviceDataService);
+    }
+
+    public void setData(List<MotorcycleDataMotorcycle> motorcyclesDataHolder, List<ServiceDataService> servicesDataHolder){
+        this.motorcyclesDataHolder = motorcyclesDataHolder;
+        this.servicesDataHolder = servicesDataHolder;
+        notifyDataSetChanged();
+    }
+}
