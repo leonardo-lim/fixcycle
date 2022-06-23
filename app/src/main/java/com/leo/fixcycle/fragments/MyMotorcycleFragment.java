ba8b8698db99ad1afd763423556fc99a02a877bb
leonardo-lim <leonardolim178@gmail.com>
2022-06-23 22:58:18 +0700

    Check whether the motorcycle data is empty or not

--- app/src/main/java/com/leo/fixcycle/fragments/MyMotorcycleFragment.java
+++ app/src/main/java/com/leo/fixcycle/fragments/MyMotorcycleFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,7 +18,6 @@ import android.widget.Toast;
 
 import com.leo.fixcycle.R;
 import com.leo.fixcycle.activities.AddMotorcycleActivity;
-import com.leo.fixcycle.activities.LoginActivity;
 import com.leo.fixcycle.activities.MotorcycleDetailsActivity;
 import com.leo.fixcycle.models.Motorcycle;
 import com.leo.fixcycle.adapters.MotorcycleAdapter;
@@ -73,8 +73,13 @@ public class MyMotorcycleFragment extends Fragment implements MotorcycleAdapter.
                 public void onResponse(Call<Motorcycle> call, Response<Motorcycle> response) {
                     if (response.isSuccessful() && response.body() != null) {
                         List<MotorcycleDataMotorcycle> motorcyclesDataHolder = response.body().getData().getMotorcycles();
-                        motorcycleAdapter.setData(motorcyclesDataHolder);
-                        recyclerView.setAdapter(motorcycleAdapter);
+
+                        if (motorcyclesDataHolder.size() > 0) {
+                            ConstraintLayout noMotorcycleLayout = view.findViewById(R.id.no_motorcycle_message);
+                            noMotorcycleLayout.setVisibility(View.GONE);
+                            motorcycleAdapter.setData(motorcyclesDataHolder);
+                            recyclerView.setAdapter(motorcycleAdapter);
+                        }
                     } else if (response.errorBody() != null) {
                         try {
                             JSONObject error = new JSONObject(response.errorBody().string());
