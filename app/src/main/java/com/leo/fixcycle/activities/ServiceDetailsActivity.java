990cc390ffe2cf517aa86ec8187b79e7a9936193
leonardo-lim <leonardolim178@gmail.com>
2022-06-25 17:04:48 +0700

    Add cancel service feature

--- app/src/main/java/com/leo/fixcycle/activities/ServiceDetailsActivity.java
+++ app/src/main/java/com/leo/fixcycle/activities/ServiceDetailsActivity.java
@@ -1,37 +1,52 @@
 package com.leo.fixcycle.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.leo.fixcycle.R;
+import com.leo.fixcycle.models.Motorcycle;
 import com.leo.fixcycle.models.MotorcycleDataMotorcycle;
 import com.leo.fixcycle.models.ServiceDataService;
+import com.leo.fixcycle.networks.ServiceClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ServiceDetailsActivity extends AppCompatActivity {
     MotorcycleDataMotorcycle motorcycle;
     ServiceDataService service;
     Bundle extras;
-    Button button;
+    Button seeMotorcycleDetailsButton, cancelServiceButton;
+    int serviceId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_details);
 
-        button = findViewById(R.id.see_motorcycle_btn);
+        seeMotorcycleDetailsButton = findViewById(R.id.see_motorcycle_details_btn);
+        cancelServiceButton = findViewById(R.id.cancel_service_btn);
+
+        seeMotorcycleDetailsButton.setOnClickListener(view -> openActivity());
+        cancelServiceButton.setOnClickListener(view -> showAlertDialog());
 
         getServiceDetails();
-
-        button.setOnClickListener(v->{
-            motorcycle = (MotorcycleDataMotorcycle) getIntent().getSerializableExtra("motorcycleData");
-            Intent intent = new Intent(ServiceDetailsActivity.this,MotorcycleDetailsActivity.class).putExtra("data",motorcycle);
-            startActivity(intent);
-        });
     }
 
     public void getServiceDetails(){
@@ -47,6 +62,7 @@ public class ServiceDetailsActivity extends AppCompatActivity {
         if (extras != null) {
             motorcycle = (MotorcycleDataMotorcycle) getIntent().getSerializableExtra("motorcycleData");
             service = (ServiceDataService) getIntent().getSerializableExtra("serviceData");
+            serviceId = service.getId();
 
             String status;
             int stat =service.getServiceStatus();
@@ -59,6 +75,11 @@ public class ServiceDetailsActivity extends AppCompatActivity {
             }else{
                 status="Canceled";
             }
+
+            if (stat != 1) {
+                cancelServiceButton.setVisibility(View.GONE);
+            }
+
             String typeService;
             int type = service.getType();
             if(type==1){
@@ -78,5 +99,68 @@ public class ServiceDetailsActivity extends AppCompatActivity {
             timeContent.setText(date.substring(11,16));
             requestContent.setText(request);
         }
+    }
+
+    private void openActivity() {
+        motorcycle = (MotorcycleDataMotorcycle) getIntent().getSerializableExtra("motorcycleData");
+        Intent intent = new Intent(ServiceDetailsActivity.this, MotorcycleDetailsActivity.class);
+        intent.putExtra("data", motorcycle);
+        startActivity(intent);
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder
+                .setIcon(R.drawable.logo)
+                .setTitle("Cancel")
+                .setMessage("Are you sure want to cancel this service?")
+                .setCancelable(false)
+                .setNegativeButton("No", (dialogInterface, i) -> {
+                    dialogInterface.cancel();
+                })
+                .setPositiveButton("Yes", (dialogInterface, i) -> {
+                    deleteServiceData();
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void deleteServiceData() {
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        String accessToken = sp.getString("accessToken", "");
+
+        ServiceClient call = new ServiceClient();
+
+        call.getApi().removeService(serviceId, "Bearer " + accessToken).enqueue(new Callback<Motorcycle>() {
+            @Override
+            public void onResponse(Call<Motorcycle> call, Response<Motorcycle> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Intent intent = new Intent(ServiceDetailsActivity.this, MainActivity.class);
+                    intent.putExtra("fragmentName", "orders");
+                    startActivity(intent);
+                    showToast("Service canceled successfully");
+                    finish();
+                } else if (response.errorBody() != null) {
+                    try {
+                        JSONObject error = new JSONObject(response.errorBody().string());
+                        String message = error.getString("message");
+                        showToast(message);
+                    } catch (IOException | JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Motorcycle> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(ServiceDetailsActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
